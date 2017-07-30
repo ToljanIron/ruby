0344a077ca3683d18d7486dbb90d264d30866102
Danny Milman <danny@spectory.com>
2017-07-30 14:59:57 +0300

    sadump

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class SmsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token
 
   def receive_and_respond
     authorize :application, :passthrough
