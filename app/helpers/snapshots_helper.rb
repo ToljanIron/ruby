1e520ccea1c61ba202638c26047f63aa8a062119
Danny Milman <danny@spectory.com>
2017-09-06 12:20:55 +0300

    fixes to time picker api

--- app/helpers/snapshots_helper.rb
+++ app/helpers/snapshots_helper.rb
@@ -3,23 +3,23 @@ module SnapshotsHelper
 	EMAILS_VOLUME_ALGORITHM_ID = 707
 
   def get_emails_volume_scores(interval_type, current_gids, cid)
-  	
+
   	# Contains score for month - last snapshot of each month - each snapshot is calculating measures 4 weeks back
   	scores_per_month = []
   	res = []
   	gids = []
 
   	interval_str = get_interval_type_string(interval_type)
-		
+
     sids = get_relevant_snapshot_ids(cid, 20)
-    
+
     if (current_gids.nil? || current_gids.length === 0)
       gids << Group.get_root_group(cid)
     else
       gids = get_relevant_group_ids(sids, current_gids)
       gids = gids.map(&:to_i)
     end
-    
+
 		sqlstr = "SELECT *
 							FROM (
 						  SELECT
@@ -39,7 +39,7 @@ module SnapshotsHelper
 							ORDER BY timestamp ASC"
 
 		sqlres = ActiveRecord::Base.connection.select_all(sqlstr)
-  	
+
   	sqlres.each {|entry|
   		scores_per_month << {
   			'score'       => entry['score'],
@@ -54,13 +54,14 @@ module SnapshotsHelper
 
   def get_relevant_snapshot_ids(cid, limit)
     sqlstr = "SELECT id FROM (
-              SELECT 
+              SELECT
               snapshots.id,
               company_id,
               month,
               timestamp,
               max(timestamp) OVER (PARTITION BY month) AS max_timestamp
               FROM snapshots
+              WHERE company_id = #{cid}
               ) t
               WHERE timestamp = max_timestamp AND company_id = #{cid}
               ORDER BY timestamp ASC
@@ -70,13 +71,10 @@ module SnapshotsHelper
   end
 
   def get_relevant_group_ids(sids, current_gids)
-
     res = []
-
     sids.each do |sid|
-      current_gids.each do |gid|
-        res << Group.find_group_in_snapshot(gid, sid)
-      end
+      grp = Group.find_groups_in_snapshot(current_gids, sid)
+      res += grp
     end
     return res
   end
