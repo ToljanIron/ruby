c8d2e5ce62a53db4f870e9af467cfa9c8917883e
Danny Milman <danny@spectory.com>
2017-10-19 16:27:29 +0300

    change bottlenecks algo type to 1

--- db/seeds/algorithms.rb
+++ db/seeds/algorithms.rb
@@ -25,7 +25,7 @@ Algorithm.find_or_create_by!(id: 710, name: 'external_senders_measure', algorith
 
 ############## Meetings ####################
 Algorithm.find_or_create_by!(id: 100, name: 'calculate_information_isolate_to_args', algorithm_type_id: 2, algorithm_flow_id: 1, meaningful_sqew: Algorithm::SCORE_SKEW_HIGH_IS_BAD)
-Algorithm.find_or_create_by!(id: 130, name: 'calculate_bottlenecks', algorithm_type_id: 2, algorithm_flow_id: 1, meaningful_sqew: Algorithm::SCORE_SKEW_HIGH_IS_BAD)
+Algorithm.find_or_create_by!(id: 130, name: 'calculate_bottlenecks', algorithm_type_id: 1, algorithm_flow_id: 1, meaningful_sqew: Algorithm::SCORE_SKEW_HIGH_IS_BAD)
 Algorithm.find_or_create_by!(id: 157, name: 'average_no_of_attendees_gauge', algorithm_type_id: 5)
 Algorithm.find_or_create_by!(id: 158, name: 'proportion_time_spent_on_meetings_gauge', algorithm_type_id: 5)
 Algorithm.find_or_create_by!(id: 159, name: 'proportion_of_managers_never_in_meetings_gauge', algorithm_type_id: 5)
