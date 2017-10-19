1963fed5b0ec93747382f18de6386b7d429f3924
Danny Milman <danny@spectory.com>
2017-10-19 15:26:31 +0300

    dbugging

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -17,27 +17,32 @@ class SnapshotsController < ApplicationController
     cid = current_user.company_id
     cache_key = "get_snapshots-lim#{limit}-cid#{cid}"
     res = cache_read(cache_key)
+    puts "################## 1"
     if res.nil?
+    puts "################## 2"
       snapshots = Snapshot.where(company_id: cid, status: :active).order('timestamp DESC').limit(20)
+      puts snapshots
       res = []
       snapshots.each do |s|
         res << {sid: s.id, name: s.name}
       end
+    puts "################## 3"
       res = Oj.dump(res)
       cache_write(cache_key, res)
     end
+    puts "################## 4"
     render json: res
   end
 
   def get_time_picker_snapshots
     authorize :snapshot, :index?
-    
+
     cid = current_user.company_id
     limit = params[:limit]
 
     res = get_last_snapshots_of_each_month(cid, limit)
     res = Oj.dump(res)
-    
+
     render json: res
   end
 
