ac515ac12f31a4476ac7fa0c6b5aed3242c1a4ce
danny-sa <danny@step-ahead.com>
2020-12-04 18:02:59 +0200

    Fix buttons

--- app/assets/javascripts/mobile/controllers/mobile_questionnaire_controller.js
+++ app/assets/javascripts/mobile/controllers/mobile_questionnaire_controller.js
@@ -238,11 +238,16 @@ angular.module('workships-mobile')
   };
 
   $scope.canFinish = function () {
+    
     if (mass.is_funnel_question) {
       return mass.num_replies_true >= mass.client_min_replies &&
              mass.num_replies_true <= mass.client_max_replies;
     }
     var num_reps = mass.num_replies_true + mass.num_replies_false;
+    // console.log('&&&&&&&&&&&&&&&&&&&&&&&')
+    // console.log(mass)
+    // console.log('num_reps: ', num_reps, ', mass.client_max_replies: ', mass.client_max_replies)
+    // console.log('&&&&&&&&&&&&&&&&&&&&&&&')
     return num_reps === mass.client_max_replies;
   };
 
