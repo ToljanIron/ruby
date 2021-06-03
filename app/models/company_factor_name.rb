cf3ea0de68bab58d5fc11a8f4e3c992e2ab76f38
giti <gitiyaari@gmail.com>
2021-06-03 16:07:24 +0300

    calc questionnaire algorithm with new factors

--- app/models/company_factor_name.rb
+++ app/models/company_factor_name.rb
@@ -1,6 +1,7 @@
 class CompanyFactorName < ApplicationRecord
     
     def  self.insert_factors(cid,sid)
+        self.where(company_id: cid, snapshot_id: sid).delete_all
         Factor.all.each do |f|
             self.create(factor_name: f.name, company_id:cid, snapshot_id: sid)
         end
--- lib/tasks/modules/precalculate_network_metrics_helper.rb
+++ lib/tasks/modules/precalculate_network_metrics_helper.rb
@@ -13,16 +13,16 @@ K = 1
       emps.gender,
       emps.group_id as group,
       emps.rank_id as rank,
-      emps.param_a_id as param_a,
-      emps.param_b_id as param_b,
-      emps.param_c_id as param_c,
-      emps.param_d_id as param_d,
-      emps.param_e_id as param_e,
-      emps.param_f_id as param_f,
-      emps.param_g_id as param_g,
-      emps.param_h,
-      emps.param_i,
-      emps.param_j,
+      emps.factor_a_id as param_a,
+      emps.factor_b_id as param_b,
+      emps.factor_c_id as param_c,
+      emps.factor_d_id as param_d,
+      emps.factor_e_id as param_e,
+      emps.factor_f_id as param_f,
+      emps.factor_g_id as param_g,
+      emps.factor_h as param_h,
+      emps.factor_i as param_i,
+      emps.factor_j as param_j
       ")
       .from("employees emps")
       .joins("left join groups g on emps.group_id = g.id")
@@ -32,7 +32,10 @@ K = 1
     n = participants.length
     base_mat[0] = Array.new(n+1,0)
     emps_hash = {}
-    paramsArr = ['office','gender', 'group','rank','param_a','param_b','param_c','param_d','param_e','param_f','param_g','param_h','param_i','param_j']
+    paramsScore = ['office','gender', 'group','rank','param_a','param_b','param_c','param_d','param_e','param_f','param_g','param_h','param_i','param_j']
+    conn_hash = {}
+    paramsScore.each{|p_s| conn_hash[p_s.to_sym] = {}}
+  # paramsScore = ['office','gender', 'group','rank']    
 
     participants.each_with_index do |val,idx|
       unless base_mat[idx+1] 
@@ -76,15 +79,25 @@ K = 1
       matD = base_mat.clone.map(&:clone)
       matE = base_mat.clone.map(&:clone)
 
+      matPA = base_mat.clone.map(&:clone)
+      matPB = base_mat.clone.map(&:clone)
+      matPC = base_mat.clone.map(&:clone)
+      matPD = base_mat.clone.map(&:clone)
+      matPE = base_mat.clone.map(&:clone)
+      matPF = base_mat.clone.map(&:clone)
+      matPG = base_mat.clone.map(&:clone)
+      matPH = base_mat.clone.map(&:clone)
+      matPI = base_mat.clone.map(&:clone)
+      matPJ = base_mat.clone.map(&:clone)
+      paramsArr = {office: matB, gender: matC, group: matD, rank: matE, param_a: matPA, param_b: matPB,param_c: matPC, param_d: matPD, param_e: matPE, param_f: matPF, param_g: matPG, param_h: matPH,  param_i: matPI, param_j: matPJ}
 
       for i in 1...base_mat.length
         emp1 = base_mat[i][0]
         for j in 1...base_mat.length
-            emp2 = base_mat[0][j]
-            matB[i][j] = 1 if participants_score[emp1][:office][:name] == participants_score[emp2][:office][:name]
-            matC[i][j] = 1 if participants_score[emp1][:gender][:name] == participants_score[emp2][:gender][:name]
-            matD[i][j] = 1 if participants_score[emp1][:group][:name] == participants_score[emp2][:group][:name]
-            matE[i][j] = 1 if participants_score[emp1][:rank][:name] == participants_score[emp2][:rank][:name]
+          emp2 = base_mat[0][j]
+          paramsArr.each do |param,matrix|
+            matrix[i][j] = 1 if participants_score[emp1][param][:name] == participants_score[emp2][param][:name]
+          end
         end
       end
 
@@ -93,143 +106,119 @@ K = 1
       # print_matrix(matC,"mat-gender-#{q.network_id}.csv")
       # print_matrix(matD,"mat-group-#{q.network_id}.csv")
       # print_matrix(matE,"mat-rank-#{q.network_id}.csv")  
-
+      print_matrix(matPA,"matPA-Q#{q.network_id}-#{Time.now.strftime('%Y-%m-%d %H:%M')}.csv")
+      print_matrix(matPB,"matPB-Q#{q.network_id}-#{Time.now.strftime('%Y-%m-%d %H:%M')}.csv")
+      
       for i in 1...base_mat.length
         emp = base_mat[i][0]
         for j in 1...base_mat[i].length
-          participants_score[emp][:office][:selections] += matA[j][i] * matB[j][i]
-          participants_score[emp][:office][:bidirectional] += matA[j][i] * matB[j][i] + matA[i][j] * matB[i][j]
-          participants_score[emp][:office][:sum] += matB[j][i]
-          if( matA[j][i] == 1 && matB[j][i] == 0 )
-            participants_score[emp][:office][:other_selections] += 1
-          end
-
-          participants_score[emp][:gender][:selections] += matA[j][i] * matC[j][i]
-          participants_score[emp][:gender][:bidirectional] += matA[j][i] * matC[j][i] + matA[i][j] * matC[i][j]
-          participants_score[emp][:gender][:sum] += matC[j][i]
-          if( matA[j][i] == 1 && matC[j][i] == 0 )
-            participants_score[emp][:gender][:other_selections] += 1
-          end
-
-          participants_score[emp][:group][:selections] += matA[j][i] * matD[j][i]
-          participants_score[emp][:group][:bidirectional] += matA[j][i] * matD[j][i] + matA[i][j] * matD[i][j]
-          participants_score[emp][:group][:sum] += matD[i][j]
-          if( matA[j][i] == 1 && matD[j][i] == 0 )
-            participants_score[emp][:group][:other_selections] += 1
-          end
-
-          participants_score[emp][:rank][:selections] += matA[j][i] * matE[j][i]
-          participants_score[emp][:rank][:bidirectional] += matA[j][i] * matE[j][i] + matA[i][j] * matE[i][j]
-          participants_score[emp][:rank][:sum] += matE[j][i]
-          if( matA[j][i] == 1 && matE[j][i] == 0 )
-            participants_score[emp][:rank][:other_selections] += 1
-          end
-
+          paramsArr.each do |param,matrix|
+            participants_score[emp][param][:selections] += matA[j][i] * matrix[j][i]
+            participants_score[emp][param][:bidirectional] += matA[j][i] * matrix[j][i] + matA[i][j] * matrix[i][j]
+            participants_score[emp][param][:sum] += matrix[j][i]
+            if( matA[j][i] == 1 && matrix[j][i] == 0 )
+              participants_score[emp][param][:other_selections] += 1
+            end
+          end         
           participants_score[emp][:total_selections] += matA[j][i] # num of participants that choose him
           participants_score[emp][:bidirectional_total] += matA[i][j] # num of participants that choose him
         end
       end
-      insert_internal_champions_values(participants_score,q.network_id,sid,n)
-      insert_isolated_values(participants_score,q.network_id,sid,n)
-      insert_new_internal_champion(participants_score,q.network_id,sid,n)
-      insert_new_connectors(participants_score,q.network_id,sid,n)
+      insert_internal_champions_values(participants_score,q.network_id,sid,n,paramsScore)
+      insert_isolated_values(participants_score,q.network_id,sid,n,paramsScore)
+      insert_new_internal_champion(participants_score,q.network_id,sid,n,paramsScore)
+      insert_new_connectors(participants_score,q.network_id,sid,n,paramsScore)
       
       matZ = {}
       for i in 1...base_mat.length
         emp = base_mat[i][0]
-        matZ[emp] = {office: {},gender: {},group: {},rank: {}}
+        matZ[emp] = conn_hash.deep_dup  
         for j in 1...base_mat[i].length
           emp2 = participants_score[base_mat[0][j]]
-          matZ[emp][:office][emp2[:office][:name]] ||= 0
-          matZ[emp][:gender][emp2[:gender][:name]] ||= 0
-          matZ[emp][:group][emp2[:group][:name]] ||= 0
-          matZ[emp][:rank][emp2[:rank][:name]] ||= 0
-
-          matZ[emp][:office][emp2[:office][:name]] += 1 if(matA[i][j].to_i == 1 || matA[j][i].to_i == 1)
-          matZ[emp][:gender][emp2[:gender][:name]] += 1 if(matA[i][j].to_i ==1 || matA[j][i].to_i == 1)
-          matZ[emp][:group][emp2[:group][:name]] += 1 if(matA[i][j] .to_i ==1|| matA[j][i].to_i == 1)
-          matZ[emp][:rank][emp2[:rank][:name]] += 1 if(matA[i][j].to_i ==1 || matA[j][i].to_i == 1)
+          paramsScore.each do |param|
+            matZ[emp][param.to_sym][emp2[param.to_sym][:name]] ||= 0
+            matZ[emp][param.to_sym][emp2[param.to_sym][:name]] += 1 if(matA[i][j].to_i == 1 || matA[j][i].to_i == 1)
+          end
         end
-        Rails.logger.info "-------------------------------------------------"
-        Rails.logger.info("Employee: #{emp}, office: (#{matZ[emp][:office].values}), gender: (#{matZ[emp][:gender].values}), group: (#{matZ[emp][:group].values}), rank: (#{matZ[emp][:rank].values})")
-        participants_score[emp][:office][:connectors] = calc_blau_index(matZ[emp][:office],n)
-        participants_score[emp][:gender][:connectors] = calc_blau_index(matZ[emp][:gender],n)
-        participants_score[emp][:group][:connectors] = calc_blau_index(matZ[emp][:group],n)
-        participants_score[emp][:rank][:connectors] = calc_blau_index(matZ[emp][:rank],n)
+        paramsScore.each do |p_s|
+          participants_score[emp][p_s.to_sym][:connectors] = calc_blau_index(matZ[emp][p_s.to_sym],n)
+        end
+      #   Rails.logger.info "-------------------------------------------------"
+      #   Rails.logger.info("Employee: #{emp}, office: (#{matZ[emp][:office].values}), gender: (#{matZ[emp][:gender].values}), group: (#{matZ[emp][:group].values}), rank: (#{matZ[emp][:rank].values})")
       end
-      insert_connectors_values(participants_score,q.network_id,sid,n)
+      insert_connectors_values(participants_score,q.network_id,sid,n,paramsScore)
 
     end
-
   end
 
   def isolated_val(value)
     return (value == 0 ? 1 : 0)
   end
 
-  def insert_new_internal_champion(participants_score,network_id,sid,n)
+  def insert_new_internal_champion(participants_score,network_id,sid,n,paramsScore)
     algorithm_id = AlgorithmType.find_by_name('new_internal_champion').id
     participants_score.each do |emp_id,val|
-      general_score = ''
-      group_score = (val[:group][:sum]-1 > 0 ?  ((val[:group][:selections].to_f/(val[:group][:sum]-1).to_f) * (n - val[:group][:sum]) * K ) : 0).round(3)
-      office_score = (val[:office][:sum]-1 > 0 ?  ((val[:office][:selections].to_f/(val[:office][:sum]-1).to_f) * (n - val[:office][:sum]) * K ) : 0).round(3)
-      gender_score = (val[:gender][:sum]-1 > 0 ?  ((val[:gender][:selections].to_f/(val[:gender][:sum]-1).to_f) * (n - val[:gender][:sum]) * K ) : 0).round(3)
-      rank_score = (val[:rank][:sum]-1 > 0 ?  ((val[:rank][:selections].to_f/(val[:rank][:sum]-1).to_f) * (n - val[:rank][:sum]) * K ) : 0).round(3)
-
-      QuestionnaireAlgorithm.create!(:employee_id => emp_id,:algorithm_type_id => algorithm_id,:network_id => network_id, :snapshot_id => sid, :general_score => general_score, :group_score => group_score, :office_score => office_score, :gender_score => gender_score, :rank_score => rank_score)
+      qa = QuestionnaireAlgorithm.new(employee_id: emp_id, algorithm_type_id: algorithm_id, network_id: network_id, snapshot_id: sid)
+      qa.general_score = ''
+      paramsScore.each do |p_score|
+        score = (val[p_score.to_sym][:sum]-1 > 0 ?  ((val[p_score.to_sym][:selections].to_f/(val[p_score.to_sym][:sum]-1).to_f) * (n - val[p_score.to_sym][:sum]) * K ) : 0).round(3)
+        qa["#{p_score}_score"] = score
+      end
+      Rails.logger.info "EMP: #{emp_id}, Q: #{network_id},  val[:param_a][:sum] = #{val[:param_a][:sum]}, val[:param_a][:selections] = #{val[:param_a][:selections]}, val[:param_a][:other_selections]= #{val[:param_a][:other_selections]}, GROUP_SCORE= #{qa.param_a_score}"
+      qa.save!
     end
   end
 
-  def insert_new_connectors(participants_score,network_id,sid,n)
+  def insert_new_connectors(participants_score,network_id,sid,n,paramsScore)
     algorithm_id = AlgorithmType.find_by_name('new_connectors').id
     participants_score.each do |emp_id,val|
-      general_score = ''
-      group_score = (val[:group][:sum]-1 > 0 && n != val[:group][:sum] ?  ((val[:group][:other_selections].to_f/(n - val[:group][:sum]).to_f) * ( val[:group][:sum] - 1) * K ) : 0).round(3)
-      office_score = (val[:office][:sum]-1 > 0 && n != val[:office][:sum] ?  ((val[:office][:other_selections].to_f/(n - val[:office][:sum]).to_f) * ( val[:office][:sum] - 1) * K ) : 0).round(3)
-      gender_score = (val[:gender][:sum]-1 > 0 && n != val[:gender][:sum] ?  ((val[:gender][:other_selections].to_f/(n - val[:gender][:sum]).to_f) * ( val[:gender][:sum] - 1) * K ) : 0).round(3)
-      rank_score = (val[:rank][:sum]-1 > 0 && n != val[:rank][:sum] ?  ((val[:rank][:other_selections].to_f/(n - val[:rank][:sum]).to_f) * ( val[:rank][:sum] - 1) * K ) : 0).round(3)
-
-      QuestionnaireAlgorithm.create!(:employee_id => emp_id,:algorithm_type_id => algorithm_id,:network_id => network_id, :snapshot_id => sid, :general_score => general_score, :group_score => group_score, :office_score => office_score, :gender_score => gender_score, :rank_score => rank_score)
+      qa = QuestionnaireAlgorithm.new(employee_id: emp_id, algorithm_type_id: algorithm_id, network_id: network_id, snapshot_id: sid)
+      qa.general_score = ''
+      paramsScore.each do |p_score|
+        score = (val[p_score.to_sym][:sum]-1 > 0 && n != val[p_score.to_sym][:sum] ?  ((val[p_score.to_sym][:other_selections].to_f/(n - val[p_score.to_sym][:sum]).to_f) * ( val[p_score.to_sym][:sum] - 1) * K ) : 0).round(3)
+        qa["#{p_score}_score"] = score
+      end
+      Rails.logger.info "EMP: #{emp_id}, Q: #{network_id},  val[:param_a][:sum] = #{val[:param_a][:sum]}, val[:param_a][:selections] = #{val[:param_a][:selections]}, val[:param_a][:other_selections]= #{val[:param_a][:other_selections]}, ---C_GROUP_SCORE= #{qa.param_a_score}"
+      qa.save!
     end
   end
 
-
-
-  def insert_internal_champions_values(participants_score,network_id,sid,n)
+  def insert_internal_champions_values(participants_score,network_id,sid,n,paramsScore)
     algorithm_id = AlgorithmType.find_by_name("internal_champion").id
     participants_score.each do |emp_id,val|
-      general_score = (val[:total_selections].to_f/(n-1).to_f).round(3)
-      group_score = (val[:group][:sum]-1 > 0 ?  (val[:group][:selections].to_f/(val[:group][:sum]-1).to_f) : 0).round(3)
-      office_score = (val[:office][:sum]-1 > 0 ?  (val[:office][:selections].to_f/(val[:office][:sum]-1).to_f) : 0).round(3)
-      gender_score = (val[:gender][:sum]-1 > 0 ?  (val[:gender][:selections].to_f/(val[:gender][:sum]-1).to_f) : 0).round(3)
-      rank_score = (val[:rank][:sum]-1 > 0 ?  (val[:rank][:selections].to_f/(val[:rank][:sum]-1).to_f) : 0).round(3)
-
-      QuestionnaireAlgorithm.create!(:employee_id => emp_id,:algorithm_type_id => algorithm_id,:network_id => network_id, :snapshot_id => sid, :general_score => general_score, :group_score => group_score, :office_score => office_score, :gender_score => gender_score, :rank_score => rank_score)
+      qa = QuestionnaireAlgorithm.new(employee_id: emp_id, algorithm_type_id: algorithm_id, network_id: network_id, snapshot_id: sid)
+      qa.general_score = (val[:total_selections].to_f/(n-1).to_f).round(3)
+      paramsScore.each do |p_score|
+        score = (val[p_score.to_sym][:sum]-1 > 0 ?  (val[p_score.to_sym][:selections].to_f/(val[p_score.to_sym][:sum]-1).to_f) : 0).round(3)
+        qa["#{p_score}_score"] = score
+      end
+      qa.save!
     end
   end
 
-  def insert_isolated_values(participants_score,network_id,sid,n)
+  def insert_isolated_values(participants_score,network_id,sid,n,paramsScore)
     algorithm_id = AlgorithmType.find_by_name("isolated").id
     participants_score.each do |emp_id,val|
-      general_score =  (val[:bidirectional_total].to_f/(n-1).to_f).round(3)
-      group_score = isolated_val(val[:group][:bidirectional])
-      office_score = isolated_val(val[:office][:bidirectional])
-      gender_score = isolated_val(val[:gender][:bidirectional])
-      rank_score = isolated_val(val[:rank][:bidirectional])
-      QuestionnaireAlgorithm.create!(:employee_id => emp_id,:algorithm_type_id => algorithm_id,:network_id => network_id, :snapshot_id => sid, :general_score => general_score, :group_score => group_score, :office_score => office_score, :gender_score => gender_score, :rank_score => rank_score)
-
+      qa = QuestionnaireAlgorithm.new(employee_id: emp_id, algorithm_type_id: algorithm_id, network_id: network_id, snapshot_id: sid)
+      qa.general_score =  (val[:bidirectional_total].to_f/(n-1).to_f).round(3)
+      paramsScore.each do |p_score|
+        score = isolated_val(val[p_score.to_sym][:bidirectional])
+        qa["#{p_score}_score"] = score
+      end
+      qa.save!
     end
   end
 
-  def insert_connectors_values(participants_score,network_id,sid,n)
+  def insert_connectors_values(participants_score,network_id,sid,n,paramsScore)
     algorithm_id = AlgorithmType.find_by_name("connectors").id
     participants_score.each do |emp_id,val|
-      general_score = ''
-      group_score = val[:group][:connectors].round(3)
-      office_score = val[:office][:connectors].round(3)
-      gender_score = val[:gender][:connectors].round(3)
-      rank_score = val[:rank][:connectors].round(3)
-      QuestionnaireAlgorithm.create!(:employee_id => emp_id,:algorithm_type_id => algorithm_id,:network_id => network_id, :snapshot_id => sid, :general_score => general_score, :group_score => group_score, :office_score => office_score, :gender_score => gender_score, :rank_score => rank_score)
+      qa = QuestionnaireAlgorithm.new(employee_id: emp_id, algorithm_type_id: algorithm_id, network_id: network_id, snapshot_id: sid)
+      qa.general_score =  ''
+      paramsScore.each do |p_score|
+        score = val[p_score.to_sym][:connectors].round(3)
+        qa["#{p_score}_score"] = score
+      end
+      qa.save!
     end
   end
 
@@ -242,7 +231,6 @@ K = 1
     Rails.logger.info 1-calc
     return (1 - calc)
   end
-
 
   def print_matrix(matx,file_name)
     file_path = Rails.root.join('public', file_name)
@@ -264,7 +252,5 @@ K = 1
       return false
     end
   end
-
-
 
 end
