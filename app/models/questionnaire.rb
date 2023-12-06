b8e5d72879539760af58b41aa8d796597c287a2b
Uri Elhav <uelhav@gmail.com>
2023-12-06 08:34:07 -0500

    removed byebug

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -393,7 +393,6 @@ class Questionnaire < ActiveRecord::Base
     raise "No such questionnaire" if questionnaire.nil?
     temp_email=['unveified',company.name.gsub(' ',''),Time.now.utc.strftime("%Y%m%d%H%M%S")].join('-')+'@stepahead.com' 
     unverified_employee=Employee.create!(snapshot_id:questionnaire.snapshot_id,is_verified:false,group:group,email:temp_email,company_id:company.id,first_name:permitted[:e_first_name],last_name:permitted[:e_last_name],external_id:Time.now.utc.strftime("%Y%m%d%H%M%S"))
-    byebug
     unverified_participant=QuestionnaireParticipant.create!(snowballer_employee_id:snowballed_by,employee_id:unverified_employee.id,questionnaire_id:questionnaire.id,status:4,active:true)
 
     msg=(unverified_employee.errors.full_messages+unverified_participant.errors.full_messages).flatten.join(',')
